298d772e13de805309d17fa8506c2c6dac845b7c
Bruno de Carvalho <bruno@biasedbit.com>
2012-05-08 04:32:03 +0100

    Initial Commit

---
+++ BBStorageSpeedTest/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  BBStorageSpeedTest
+//
+//  Created by Bruno de Carvalho on 5/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BBStorageSpeedTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BBStorageSpeedTest
+//
+//  Created by Bruno de Carvalho on 5/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBAppDelegate class]));
+    }
+}
